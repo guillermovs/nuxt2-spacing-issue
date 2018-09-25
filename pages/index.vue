17b9bd83f960bf118d0217aa80cd4a4cbc127d9a
Guillermo <guillermo.vigil@gmail.com>
2018-09-25 12:28:50 +0100

    Display issue w/Nuxt 2
    Removed Vuetify dependency

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container">
+
+  <!-- START: These elements should be displayed inline, but a space appears in between them, breaking them into two lines -->
+    <section class="inline-block">
+      Inline-block left
+    </section>
+
+    <section class="inline-block">
+      Inline-block right
+    </section>
+    <!-- END -->
+
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+.inline-block{
+  display:inline-block;
+  width: 50%;
+}
+
+.container{
+  width:100%;
+}
+
+</style>
